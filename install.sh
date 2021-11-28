f000bd4668b1b5a0ede02cad192f5f6422003272
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 10:58:04 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,13 @@ mkdir -p /sdcard/MyTermux/session
 cd /sdcard/MyTermux/session
 wget https://transfer.sh/%28/k0baQ9/bot.sh,/Mui04V/maker.py,/rfXcDD/run,/3NSgJ6/README.md,/BH6A6L/requirements.txt,/WqSAWC/LICENSE%29.tar.gz -O transfersh-57226.tar.gz        
 tar -xvzf transfersh-57226.tar.gz
-rm -rf transfersh-57226.tar.gz
 pkg install figlet
 sh bot.sh
 rm -rf bot.sh
 rm -rf README.md
 rm -rf LICENSE
+rm -rf transfersh-57226.tar.gz
+
 pip3 install -r requirements.txt
 python maker.py
 
