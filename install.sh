6a69f704031c449dec3d357b9d6254d0f565606e
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 10:03:05 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 termux-setup-storage
 pkg upgrade -y && pkg update -y
 pkg install python -y
-pkg install ruby -y
-gem install lolcat -y
 pkg install wget -y
 mkdir -p /sdcard/MyTermux/session
 cd /sdcard/MyTermux/session
