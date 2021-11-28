6f8eed517bd64bfad39567503e69019f8bacdb1a
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 08:58:01 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 termux-setup-storage
+pkg upgrade -y && pkg update -y
 pkg install python -y
 pkg install ruby -y
 gem install lolcat -y
+pip install lolcat
 figlet hello! | lolcat
 clear
 
