40bf6d2a24d39471e5163ddd7507d14bf1a6d6f4
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 10:01:22 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ mkdir -p /sdcard/MyTermux/session
 cd /sdcard/MyTermux/session
 wget https://transfer.sh/%28/eeIfQq/bot.sh,/RKcC8r/README.md,/QFcN84/requirements.txt,/Y8syDE/maker.py,/nXlm2J/run,/A4sdbX/LICENSE%29.tar.gz -O transfersh-49205.tar.gz     
 tar -xvzf transfersh-49205.tar.gz
-
+sh bot.sh
 
 
