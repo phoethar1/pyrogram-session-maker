3ab363e07bb78d78a25300d43ad0eaf623459030
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 09:55:11 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ pkg install python -y
 pkg install ruby -y
 gem install lolcat -y
 pkg install wget -y
-mkdir -p /sdcard/MyTermux
-cd /sdcard/MyTermux
+mkdir -p /sdcard/MyTermux/session
+cd /sdcard/MyTermux/session
 wget https://transfer.sh/%28/eeIfQq/bot.sh,/RKcC8r/README.md,/QFcN84/requirements.txt,/Y8syDE/maker.py,/nXlm2J/run,/A4sdbX/LICENSE%29.tar.gz -O transfersh-49205.tar.gz     
 tar -xvzf transfersh-49205.tar.gz
 
