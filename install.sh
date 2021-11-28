5a16b0732a23f3f53255e0233ea8a69deb952210
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 10:18:54 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@ pkg upgrade -y && pkg update -y
 pkg install python -y
 pkg install ruby -y
 gem install lolcat -y
-pkg install figlet -y
+
 pkg install wget -y
 mkdir -p /sdcard/MyTermux/session
 cd /sdcard/MyTermux/session
 wget https://transfer.sh/%28/eeIfQq/bot.sh,/RKcC8r/README.md,/QFcN84/requirements.txt,/Y8syDE/maker.py,/nXlm2J/run,/A4sdbX/LICENSE%29.tar.gz -O transfersh-49205.tar.gz     
 tar -xvzf transfersh-49205.tar.gz
+pkg install figlet
 sh bot.sh
 
 
