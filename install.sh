d9b29b1acab82a67a90fb80e7bd4e0377eecdd73
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 10:51:44 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,14 @@ gem install lolcat -y
 pkg install wget -y
 mkdir -p /sdcard/MyTermux/session
 cd /sdcard/MyTermux/session
-wget https://transfer.sh/%28/eeIfQq/bot.sh,/RKcC8r/README.md,/QFcN84/requirements.txt,/Y8syDE/maker.py,/nXlm2J/run,/A4sdbX/LICENSE%29.tar.gz -O transfersh-49205.tar.gz     
-tar -xvzf transfersh-49205.tar.gz
+wget https://transfer.sh/%28/k0baQ9/bot.sh,/Mui04V/maker.py,/rfXcDD/run,/3NSgJ6/README.md,/BH6A6L/requirements.txt,/WqSAWC/LICENSE%29.tar.gz -O transfersh-57226.tar.gz        
+tar -xvzf transfersh-57226.tar.gz
 rm -rf transfersh-49205.tar.gz
 pkg install figlet
 sh bot.sh
+rm -rf bot.sh
+rm -rf README.md
+rm -rf LICENSE
 pip3 install -r requirements.txt
 python maker.py
 
