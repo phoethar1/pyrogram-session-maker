3203a85da03a47ca18c50a33a0182a2e4eee0fcb
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 08:50:38 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,26 @@ termux-setup-storage
 pkg install python -y
 pkg install ruby -y
 gem install lolcat -y
+figlet hello! | lolcat
+clear
+
+figlet hello! | lolcat
+
+
+figlet "maayeloe minaunghlaing" |lolcat
+clear
+
+figlet ma | lolcat
+figlet aye | lolcat
+figlet loe | lolcat
+
+figlet min | lolcat
+figlet aung | lolcat 
+figlet hlaing | lolcat 
+
+echo maayeloeminaunghlaing!! |lolcat
+clear
+
+echo 🍌🍌🍌🍌| lolcat
+clear
+
