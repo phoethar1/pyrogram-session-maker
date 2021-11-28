1c61afddbc3b545498c4cabe187a9d7925c24433
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-28 09:06:08 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,27 +4,4 @@ pkg upgrade -y && pkg update -y
 pkg install python -y
 pkg install ruby -y
 gem install lolcat -y
-pip install lolcat
-figlet hello! | lolcat
-clear
-
-figlet hello! | lolcat
-
-
-figlet "maayeloe minaunghlaing" |lolcat
-clear
-
-figlet ma | lolcat
-figlet aye | lolcat
-figlet loe | lolcat
-
-figlet min | lolcat
-figlet aung | lolcat 
-figlet hlaing | lolcat 
-
-echo maayeloeminaunghlaing!! |lolcat
-clear
-
-echo 🍌🍌🍌🍌| lolcat
-clear
 
